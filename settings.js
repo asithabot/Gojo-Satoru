2bfe14c5f1dfc5ad568632dafa555dc38eff1822
asithabot <103774924+asithabot@users.noreply.github.com>
2022-06-07 11:20:00 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,20 +19,20 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['94705675072']
+global.premium = ['94705675072']
+global.ownernomer = '94705675072'
+global.ownername = '🌹ᶜʰᵘᵗⁱ_ᴀꜱɪ🤫 MD✅'
+global.botname = '🌹ᶜʰᵘᵗⁱ_ᴀꜱɪ🤫 MD✅'
+global.footer = ' 🌹ᶜʰᵘᵗⁱ_ᴀꜱɪ🤫 MD✅'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = '🌹ᶜʰᵘᵗⁱ_ᴀꜱɪ😘'
+global.author = '🐦bot'
 global.sessionName = 'session'
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','😾','😘','🗿']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
